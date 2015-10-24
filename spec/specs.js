947084f0e4a259a1c94e3aaf298e45a5d3b63ccd
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-24 10:56:01 -0700

    verify two different array indexes in song output.

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,7 @@
-describe('sing', function() {
-  it("sing the 99 beers song with your number as starting point", function() {
-    var expected = beerSong(5);
-    expect(expected[0]).to.equal("5 bottles of beer on the wall, 5 bottles of beer. Take one down and pass it around, 4 bottles of beer on the wall.");
+describe('songOutput', function() {
+  it("verifies that the song output is expected based on input", function() {
+    var lyrics = beerSong(5);
+    expect(lyrics[0]).to.equal("5 bottles of beer on the wall, 5 bottles of beer. Take one down and pass it around, 4 bottles of beer on the wall.");
+    expect(lyrics[5]).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 5 bottles of beer on the wall.");
   });
 });
