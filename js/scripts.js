7df629b971fa0742c749e8912ccece2cb58429d6
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-24 10:20:17 -0700

    complete first test with correct song returned.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 var beerSong = function(n) {
-  return n;
+  while (n > 0) {
+    return (n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n -1) + " bottles of beer on the wall.");
+    n--;
+  };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe('', function() {
-  it("", function() {
-    expect(("")).to.equal("");
+describe('sing', function() {
+  it("sing the 99 beers song with your number as starting point", function() {
+    expect(beerSong(10)).to.equal("10 bottles of beer on the wall, 10 bottles of beer. Take one down and pass it around, 9 bottles of beer on the wall.");
   });
 });
