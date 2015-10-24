aec32285644f401a68967bf9980bbb63a30eb6f7
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-24 11:06:14 -0700

    include feature to make sure people don't drink too much :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 var beerSong = function(n) {
+  // Since we're manipulating n in loop I'll store the original input for reference
+  var originalInput = n;
   var lyricArray = [];
+
+  if (n > 99) {
+    return "I think you've had enough.";
+  };
+
+  // Loop to build the song
   while (n > 0) {
     lyricArray.push(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n -1) + " bottles of beer on the wall.");
     n--;
+
     if (n === 0) {
-      break;
+      lyricArray.push("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, " + originalInput + " bottles of beer on the wall.")
     };
   };
+
+  // Return output as an array
   return lyricArray;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,21 @@
 describe('songOutput', function() {
-  it("verifies that the song output is expected based on input", function() {
+  it("verifies that the song output is expected based on input of 5", function() {
     var lyrics = beerSong(5);
+
     expect(lyrics[0]).to.equal("5 bottles of beer on the wall, 5 bottles of beer. Take one down and pass it around, 4 bottles of beer on the wall.");
     expect(lyrics[5]).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 5 bottles of beer on the wall.");
+  });
+
+  it("verifies that the song output is expected based on input of 99", function() {
+    var lyrics = beerSong(99);
+
+    expect(lyrics[0]).to.equal("99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.");
+    expect(lyrics[99]).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+});
+
+describe('checkLimit', function() {
+  it("make sure everybody stays cool", function() {
+    expect(beerSong(100)).to.equal("I think you've had enough.");
   });
 });
