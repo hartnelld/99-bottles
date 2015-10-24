972c497c260497cc3569bc3d24013d5e82f699e1
Danny Hartnell <danielhartnell+epicodus@gmail.com>
2015-10-24 10:50:02 -0700

    include arrays to easily write tests against this song.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 var beerSong = function(n) {
+  var lyricArray = [];
   while (n > 0) {
-    return (n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n -1) + " bottles of beer on the wall.");
+    lyricArray.push(n + " bottles of beer on the wall, " + n + " bottles of beer. Take one down and pass it around, " + (n -1) + " bottles of beer on the wall.");
     n--;
+    if (n === 0) {
+      break;
+    };
   };
+  return lyricArray;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,6 @@
 describe('sing', function() {
   it("sing the 99 beers song with your number as starting point", function() {
-    expect(beerSong(10)).to.equal("10 bottles of beer on the wall, 10 bottles of beer. Take one down and pass it around, 9 bottles of beer on the wall.");
+    var expected = beerSong(5);
+    expect(expected[0]).to.equal("5 bottles of beer on the wall, 5 bottles of beer. Take one down and pass it around, 4 bottles of beer on the wall.");
   });
 });
